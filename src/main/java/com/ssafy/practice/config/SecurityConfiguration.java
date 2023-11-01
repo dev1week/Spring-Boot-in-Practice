786cc6bd6a673d44a07a2eac69de1915a800adec
dev1week <rondo2860@gmail.com>
2023-11-02 00:19:37 +0900

    feat: 간단한 로그인 로그아웃 구현(#1)

--- src/main/java/com/ssafy/practice/config/SecurityConfiguration.java
+++ src/main/java/com/ssafy/practice/config/SecurityConfiguration.java
@@ -1,20 +1,61 @@
 package com.ssafy.practice.config;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    private final AccessDeniedHandler customAccessDeniedHandler;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 해당 메서드를 재정의할 경우 InMemoryUserDetailsManager 설정을 변경한다.
+     * user와 admin 계정을 생성하고 각각 고정된 비밀번호를 사용하도록 설정한다.
+     *
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
+                .withUser("user")
+                .password(passwordEncoder().encode("p@ssw0rd"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder().encode("pa$$w0rd"))
+                .roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .antMatchers("/login").permitAll()
+                //delete 엔드포인트를 admin 권한을 가진 유저만 사용할 수 있도록 정의
+                .antMatchers("/delete/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login");
+                .formLogin().loginPage("/login")
+                .and()
+                //예외 상황이 발생할 경우 다음 Exception을 던진다.
+                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);
     }
 
     @Override
--- src/main/java/com/ssafy/practice/controller/LoginController.java
+++ src/main/java/com/ssafy/practice/controller/LoginController.java
@@ -1,7 +1,14 @@
 package com.ssafy.practice.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -11,4 +18,22 @@ public class LoginController {
     public String login(){
         return "login";
     }
+
+
+
+    //logout에는 csrf 공격을 막기 위해 get이 아니라 post 메서드를 사용한다.
+    @PostMapping("/doLogout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if(auth != null){
+            //SecurityContextLogoutHandler는 현재 존재하는 httpSession 객체를 비활성화하고
+            //SecurityContext에서 인증 정보를 삭제하면서 사용자를 로그아웃 처리한다.
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login";
+    }
+
+
 }
