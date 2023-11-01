220be1192bb4ad252161aa29313ee805c26b00b7
dev1week <rondo2860@gmail.com>
2023-11-02 00:58:13 +0900

    feat: jdbc 인증을 적용하여 db에 접근하여 유저 정보를 가져오자(#1)
    기존에는 자바 소스코드로 유저를 직접 등록했지만, 보안상 문제가 크고 회원가입을 하기 매우 매우 불편하기 때문에 db와 연동하여 인증하는 로직으로 바꿈

--- src/main/java/com/ssafy/practice/config/SecurityConfiguration.java
+++ src/main/java/com/ssafy/practice/config/SecurityConfiguration.java
@@ -8,65 +8,29 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final DataSource dataSource;
 
     private final AccessDeniedHandler customAccessDeniedHandler;
 
-
     @Bean
     public PasswordEncoder passwordEncoder(){
-        //내부적으로 안정성이 높은 DelegatingPasswordEncoder 인스턴스를 생성해서 PasswordEncoder 빈을 생성한다.
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
-    /**
-     * DelegatingPasswordEncoder 인스턴스를 사용해서 비밀번호를 암호화하는 방법
-     * InMemoryUserDetailsManager를 생성하는 또 다른 방법
-     *
-     * @param auth
-     * @throws Exception
-     */
-    @Bean
     @Override
-    protected UserDetailsService userDetailsService() {
-        //UserDetails 객체를 생성하여 사용자 정보를 담아준다.
-        UserDetails user = User.withUsername("user")
-                //자바 8의 메서드 레퍼런스를 사용
-                //PasswordEncoder의 encode() 메서드를 인자로 전달하면서 passwordEncoder를 등록
-                .passwordEncoder(passwordEncoder()::encode)
-                .password("p@ssw0rd")
-                .roles("USER")
-                .build();
-
-        //UserDetails 객체를 생성하여 어드민 정보를 담아준다.
-        UserDetails admin = User.withUsername("admin")
-                .passwordEncoder(passwordEncoder()::encode)
-                .password("pa$$w0rd")
-                .roles("ADMIN")
-                .build();
-
-
-        //인메모리에서 UserDetails 객체를 관리하는 매니저를 만든다.
-        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-
-
-        //생성한 유저정보를 InMemoryUserDetailsManager에게 등록한다.
-        userDetailsManager.createUser(user);
-        userDetailsManager.createUser(admin);
-
-        return userDetailsManager;
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //주입 받은 데이터 소스를 사용해서 jdbc 인증을 수행하도록 AuthenticaionManager를 설정한다.
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
