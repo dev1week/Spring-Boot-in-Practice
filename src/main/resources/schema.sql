26961a47491db6edb0ec31c997c1151bbbb788c7
dev1week <rondo2860@gmail.com>
2023-11-04 21:39:02 +0900

    feat: 이메일인증에 필요한 프론트 및 sql (#2)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,12 +7,23 @@ CREATE TABLE courses (
                          rating      INTEGER NOT NULL,
                          PRIMARY KEY (id)
 );
-create table users(
+DROP TABLE IF EXISTS users;
+create table USERS(
                          id BIGINT NOT NULL auto_increment,
                          first_name varchar(50),
                          last_name varchar(50),
                          email varchar(50),
                          username varchar(50),
                          password varchar(100),
+                         verified SMALLINT CHECK (verified IN (0, 1)),
                          PRIMARY KEY (id)
 );
+
+CREATE SEQUENCE ct_email_verifications;
+
+DROP TABLE IF EXISTS CT_EMAIL_VERIFICATIONS;
+create table CT_EMAIL_VERIFICATIONS(
+    verification_id varchar(50),
+    username varchar(50),
+    PRIMARY KEY (verification_id)
+)
